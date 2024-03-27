790d16698acf09ef1089730d03664e16c0df446d
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-03-27 18:31:35 -0500

    created public directory for frontend client

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
+// required dependencies
 const express = require('express');
 const app = express();
 const PORT = 8080;
 
+// middleware to parse the JSON body
+app.use(express.json());
+
+// GET - /api/health - check if server is up
 app.get('/api/health', (req, res) => {
     res.send("✅OKAY✅")
 });
 
+// serve static files(html, css, js, etc.)
+app.use(express.static('public'));
+
+// POST - /api/login - login a user
+app.post('/api/login', (req, res) => {
+    console.log(req.body);
+    res.json({
+        message: "User is now logged in",
+        user: req.body.username,
+        password: req.body.password
+    });
+});
+
+// running server
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
 });
