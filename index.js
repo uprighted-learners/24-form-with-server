0cdb0460513dc86c0e6f1a5781f31d8bd26a0d1f
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-03-27 19:37:56 -0500

    added cors

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 // required dependencies
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = 8080;
 
 // middleware to parse the JSON body
 app.use(express.json());
+app.use(cors(
+    {
+        origin: 'http://127.0.0.1:5501',
+    }
+));
 
 // GET - /api/health - check if server is up
 app.get('/api/health', (req, res) => {
@@ -21,6 +27,12 @@ app.post('/api/login', (req, res) => {
     res.json({
         message: "User is now logged in"
     });
+});
+
+// GET - /api/params - demonstrate query params
+app.get('/api', (req, res) => {
+    console.log(req.query);
+    res.json(req.query);
 });
 
 // running server
